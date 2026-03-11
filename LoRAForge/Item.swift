91ddd6060b4d6381ac25839966d4fca159a7d103
euphoriacyberware-ai <brian.cantin@gmail.com>
2026-03-11 17:18:17 -0400

    Initial Commit

---
+++ LoRAForge/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  LoRAForge
+//
+//  Created by Brian Cantin on 2026-03-11.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ LoRAForgeTests/LoRAForgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoRAForgeTests.swift
+//  LoRAForgeTests
+//
+//  Created by Brian Cantin on 2026-03-11.
+//
+
+import Testing
+@testable import LoRAForge
+
+struct LoRAForgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
