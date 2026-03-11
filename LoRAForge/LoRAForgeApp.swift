a264b748e1bb00e4db27644962cdd729683cd0b7
euphoriacyberware-ai <brian.cantin@gmail.com>
2026-03-11 17:43:00 -0400

    Added application menu

--- LoRAForge/LoRAForgeApp.swift
+++ LoRAForge/LoRAForgeApp.swift
@@ -2,11 +2,137 @@ import AppKit
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        buildMainMenu()
+    }
+
     func applicationDidFinishLaunching(_ notification: Notification) {
         // NSDocumentController handles document lifecycle based on Info.plist
     }
 
     func applicationShouldOpenUntitledFile(_ sender: NSApplication) -> Bool {
         true
+    }
+
+    // MARK: - Main Menu
+
+    private func buildMainMenu() {
+        let mainMenu = NSMenu()
+
+        // App menu
+        let appMenuItem = NSMenuItem()
+        mainMenu.addItem(appMenuItem)
+        let appMenu = NSMenu()
+        appMenuItem.submenu = appMenu
+        appMenu.addItem(withTitle: "About LoRAForge",
+                        action: #selector(NSApplication.orderFrontStandardAboutPanel(_:)),
+                        keyEquivalent: "")
+        appMenu.addItem(.separator())
+        let servicesItem = NSMenuItem(title: "Services", action: nil, keyEquivalent: "")
+        let servicesMenu = NSMenu(title: "Services")
+        servicesItem.submenu = servicesMenu
+        NSApp.servicesMenu = servicesMenu
+        appMenu.addItem(servicesItem)
+        appMenu.addItem(.separator())
+        appMenu.addItem(withTitle: "Hide LoRAForge",
+                        action: #selector(NSApplication.hide(_:)),
+                        keyEquivalent: "h")
+        let hideOthersItem = appMenu.addItem(withTitle: "Hide Others",
+                                             action: #selector(NSApplication.hideOtherApplications(_:)),
+                                             keyEquivalent: "h")
+        hideOthersItem.keyEquivalentModifierMask = [.command, .option]
+        appMenu.addItem(withTitle: "Show All",
+                        action: #selector(NSApplication.unhideAllApplications(_:)),
+                        keyEquivalent: "")
+        appMenu.addItem(.separator())
+        appMenu.addItem(withTitle: "Quit LoRAForge",
+                        action: #selector(NSApplication.terminate(_:)),
+                        keyEquivalent: "q")
+
+        // File menu
+        let fileMenuItem = NSMenuItem()
+        mainMenu.addItem(fileMenuItem)
+        let fileMenu = NSMenu(title: "File")
+        fileMenuItem.submenu = fileMenu
+        fileMenu.addItem(withTitle: "New",
+                         action: #selector(NSDocumentController.newDocument(_:)),
+                         keyEquivalent: "n")
+        fileMenu.addItem(withTitle: "Open…",
+                         action: #selector(NSDocumentController.openDocument(_:)),
+                         keyEquivalent: "o")
+        fileMenu.addItem(withTitle: "Open Recent", action: nil, keyEquivalent: "")
+        let recentMenu = NSMenu(title: "Open Recent")
+        recentMenu.addItem(withTitle: "Clear Menu",
+                           action: #selector(NSDocumentController.clearRecentDocuments(_:)),
+                           keyEquivalent: "")
+        fileMenu.items.last?.submenu = recentMenu
+        fileMenu.addItem(.separator())
+        fileMenu.addItem(withTitle: "Close",
+                         action: #selector(NSWindow.performClose(_:)),
+                         keyEquivalent: "w")
+        fileMenu.addItem(withTitle: "Save…",
+                         action: #selector(NSDocument.save(_:)),
+                         keyEquivalent: "s")
+        fileMenu.addItem(withTitle: "Duplicate",
+                         action: #selector(NSDocument.duplicate(_:)),
+                         keyEquivalent: "")
+        fileMenu.addItem(withTitle: "Revert to Saved",
+                         action: #selector(NSDocument.revertToSaved(_:)),
+                         keyEquivalent: "")
+
+        // Edit menu
+        let editMenuItem = NSMenuItem()
+        mainMenu.addItem(editMenuItem)
+        let editMenu = NSMenu(title: "Edit")
+        editMenuItem.submenu = editMenu
+        editMenu.addItem(withTitle: "Undo",
+                         action: Selector(("undo:")),
+                         keyEquivalent: "z")
+        editMenu.addItem(withTitle: "Redo",
+                         action: Selector(("redo:")),
+                         keyEquivalent: "Z")
+        editMenu.addItem(.separator())
+        editMenu.addItem(withTitle: "Cut",
+                         action: #selector(NSText.cut(_:)),
+                         keyEquivalent: "x")
+        editMenu.addItem(withTitle: "Copy",
+                         action: #selector(NSText.copy(_:)),
+                         keyEquivalent: "c")
+        editMenu.addItem(withTitle: "Paste",
+                         action: #selector(NSText.paste(_:)),
+                         keyEquivalent: "v")
+        editMenu.addItem(withTitle: "Select All",
+                         action: #selector(NSText.selectAll(_:)),
+                         keyEquivalent: "a")
+
+        // Window menu
+        let windowMenuItem = NSMenuItem()
+        mainMenu.addItem(windowMenuItem)
+        let windowMenu = NSMenu(title: "Window")
+        windowMenuItem.submenu = windowMenu
+        windowMenu.addItem(withTitle: "Minimize",
+                           action: #selector(NSWindow.performMiniaturize(_:)),
+                           keyEquivalent: "m")
+        windowMenu.addItem(withTitle: "Zoom",
+                           action: #selector(NSWindow.performZoom(_:)),
+                           keyEquivalent: "")
+        windowMenu.addItem(.separator())
+        windowMenu.addItem(withTitle: "Bring All to Front",
+                           action: #selector(NSApplication.arrangeInFront(_:)),
+                           keyEquivalent: "")
+        NSApp.windowsMenu = windowMenu
+
+        // Help menu
+        let helpMenuItem = NSMenuItem()
+        mainMenu.addItem(helpMenuItem)
+        let helpMenu = NSMenu(title: "Help")
+        helpMenuItem.submenu = helpMenu
+        helpMenu.addItem(withTitle: "LoRAForge Help",
+                         action: #selector(NSApplication.showHelp(_:)),
+                         keyEquivalent: "?")
+        NSApp.helpMenu = helpMenu
+
+        NSApp.mainMenu = mainMenu
     }
 }
